a3e2bccba0b243264890fb5681f1a8c678079c66
hakantaskin <mr.hakantaskin@gmail.com>
2016-08-03 14:50:39 +0300

    last commit

--- app/helpers/quick.js
+++ app/helpers/quick.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 
 export var error_log = function (log) {
-  fs.appendFile('error_log.txt',  "\n" + log, (err) => {
+  fs.appendFile('error_log.txt',  "\r\n" + log, (err) => {
   if (err) throw err;
   console.error(log);
   });
 }
 
 export var info_log = function (log) {
-  fs.appendFile('info_log.txt', "\n" + log, (err) => {
+  fs.appendFile('info_log.txt', "\r\n" + log, (err) => {
   if (err) throw err;
   console.log(log);
   });
